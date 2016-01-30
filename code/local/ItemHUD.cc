004cd56a88761682c9c8ce8b13ee9b9a0ba8efb3
Julien Bernard <julien.bernard@univ-fcomte.fr>
2016-01-30 22:41:51 +0100

    replace colored rectangles with nice textures

--- code/local/ItemHUD.cc
+++ code/local/ItemHUD.cc
@@ -17,7 +17,15 @@ namespace huaca {
   , m_rune2(false)
   , m_rune3(true)
   {
+    m_key0Texture = gResourceManager().getTexture("images/key_iron.png");
+    m_key1Texture = gResourceManager().getTexture("images/key_bronze.png");
+    m_key2Texture = gResourceManager().getTexture("images/key_silver.png");
+    m_key3Texture = gResourceManager().getTexture("images/key_gold.png");
 
+    m_rune0Texture = gResourceManager().getTexture("images/rune0_yellow.png");
+    m_rune1Texture = gResourceManager().getTexture("images/rune1_red.png");
+    m_rune2Texture = gResourceManager().getTexture("images/rune2_green.png");
+    m_rune3Texture = gResourceManager().getTexture("images/rune3_purple.png");
   }
 
   void ItemHUD::update(float dt) {
@@ -26,11 +34,12 @@ namespace huaca {
 
   static constexpr float PADDING = 20.0f;
 
-  static constexpr float KEY_WIDTH = 10.0f;
-  static constexpr float KEY_HEIGHT = 40.0f;
-  static constexpr float KEY_SPACE = 15.0f;
+  static constexpr float KEY_SIZE = 30.0f;
+  static constexpr float KEY_TEXTURE_SIZE = 64.0f;
+  static constexpr float KEY_SPACE = 10.0f;
 
-  static constexpr float RUNE_SIZE = 30.0f;
+  static constexpr float RUNE_SIZE = 50.0f;
+  static constexpr float RUNE_TEXTURE_SIZE = 64.0f;
   static constexpr float RUNE_SPACE = 10.0f;
 
   static constexpr float PORTAL_RADIUS = 20.0f;
@@ -38,43 +47,73 @@ namespace huaca {
 
   void ItemHUD::render(sf::RenderWindow& window) {
     {
-      float x = PADDING;
-      float y = PADDING;
 
-      for (auto key : { m_key0, m_key1, m_key2, m_key3 }) {
-        sf::RectangleShape shape({ KEY_WIDTH, KEY_HEIGHT });
-        shape.setPosition({ x, y });
-        shape.setFillColor(sf::Color(0x00, 0xFF, 0x00, key ? 0xFF : 0x20));
-        window.draw(shape);
+      {
+        float x = PADDING;
+        float y = PADDING;
 
-        x += KEY_WIDTH + KEY_SPACE;
+        sf::Sprite sprite;
+        sprite.setScale(KEY_SIZE / KEY_TEXTURE_SIZE, KEY_SIZE / KEY_TEXTURE_SIZE);
+
+        // key 0
+        sprite.setTexture(*m_key0Texture);
+        sprite.setPosition({ x, y });
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_key0 ? 0xFF : 0x20));
+        window.draw(sprite);
+
+        // key 1
+        x += KEY_SIZE + KEY_SPACE;
+        sprite.setTexture(*m_key1Texture);
+        sprite.setPosition({ x, y });
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_key1 ? 0xFF : 0x20));
+        window.draw(sprite);
+
+        // key 2
+        x += KEY_SIZE + KEY_SPACE;
+        sprite.setTexture(*m_key2Texture);
+        sprite.setPosition({ x, y });
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_key2 ? 0xFF : 0x20));
+        window.draw(sprite);
+
+        // key 3
+        x += KEY_SIZE + KEY_SPACE;
+        sprite.setTexture(*m_key3Texture);
+        sprite.setPosition({ x, y });
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_key3 ? 0xFF : 0x20));
+        window.draw(sprite);
+
       }
 
       {
         float x = PADDING;
         float y = gWindowGeometry().getYFromBottom(RUNE_SIZE + RUNE_SPACE + RUNE_SIZE + PADDING);
 
-        sf::RectangleShape shape({ RUNE_SIZE, RUNE_SIZE });
+        sf::Sprite sprite;
+        sprite.setScale(RUNE_SIZE / RUNE_TEXTURE_SIZE, RUNE_SIZE / RUNE_TEXTURE_SIZE);
 
         // rune 0
-        shape.setPosition(x, y);
-        shape.setFillColor(sf::Color(0xFF, 0x00, 0x00, m_rune0 ? 0xFF : 0x20));
-        window.draw(shape);
+        sprite.setTexture(*m_rune0Texture);
+        sprite.setPosition(x, y);
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_rune0 ? 0xFF : 0x20));
+        window.draw(sprite);
 
         // rune 1
-        shape.setPosition(x + RUNE_SIZE + RUNE_SPACE, y);
-        shape.setFillColor(sf::Color(0x00, 0xFF, 0x00, m_rune1 ? 0xFF : 0x20));
-        window.draw(shape);
+        sprite.setTexture(*m_rune1Texture);
+        sprite.setPosition(x + RUNE_SIZE + RUNE_SPACE, y);
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_rune1 ? 0xFF : 0x20));
+        window.draw(sprite);
 
         // rune 2
-        shape.setPosition(x, y + RUNE_SIZE + RUNE_SPACE);
-        shape.setFillColor(sf::Color(0x00, 0x00, 0xFF, m_rune2 ? 0xFF : 0x20));
-        window.draw(shape);
+        sprite.setTexture(*m_rune2Texture);
+        sprite.setPosition(x, y + RUNE_SIZE + RUNE_SPACE);
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_rune2 ? 0xFF : 0x20));
+        window.draw(sprite);
 
         // rune 3
-        shape.setPosition(x + RUNE_SIZE + RUNE_SPACE, y + RUNE_SIZE + RUNE_SPACE);
-        shape.setFillColor(sf::Color(0xFF, 0xFF, 0x00, m_rune3 ? 0xFF : 0x20));
-        window.draw(shape);
+        sprite.setTexture(*m_rune3Texture);
+        sprite.setPosition(x + RUNE_SIZE + RUNE_SPACE, y + RUNE_SIZE + RUNE_SPACE);
+        sprite.setColor(sf::Color(0xFF, 0xFF, 0xFF, m_rune3 ? 0xFF : 0x20));
+        window.draw(sprite);
 
       }
 
