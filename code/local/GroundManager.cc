1fc539220b1848b8229048461a8623160bc98e9a
Rhine25 <celia.dehors@gmail.com>
2016-01-31 14:43:42 +0100

    Added textures

--- code/local/GroundManager.cc
+++ code/local/GroundManager.cc
@@ -1,11 +1,21 @@
 #include "GroundManager.h"
-
+#include "Singletons.h"
 #include "Constants.h"
 
 namespace huaca {
+    
+  static constexpr int TILEWIDTH = 256;
+  static constexpr int TILEHEIGHT = 128;
+  
   GroundManager::GroundManager()  
   : game::Entity(1)
-  ,  m_vertices(sf::Quads) {
+  , m_vertices(sf::Quads) {
+    // Load resources
+    {
+      auto texture = gResourceManager().getTexture("images/groundWalls.png");
+      texture->setSmooth(true);
+      m_groundsWallsTexture = texture;
+    }
   }
 
   void GroundManager::addGround(sf::Vector2f coordTile, std::size_t tile) {
@@ -22,11 +32,12 @@ namespace huaca {
     quad[2].position = sf::Vector2f((coordTile.x + 1) * TILE_SIZE, (coordTile.y + 1) * TILE_SIZE);
     quad[3].position = sf::Vector2f(coordTile.x * TILE_SIZE, (coordTile.y + 1) * TILE_SIZE);
 
-    // Define color (DEBUG)
-    quad[0].color = sf::Color::White;
-    quad[1].color = sf::Color::White;
-    quad[2].color = sf::Color::White;
-    quad[3].color = sf::Color::White;
+    // Define texture
+    int randNb = m_random.computeUniformInteger(0,6);
+    quad[0].texCoords = sf::Vector2f((randNb) * TILEWIDTH, 0);
+    quad[1].texCoords = sf::Vector2f((randNb + 1) * TILEWIDTH, 0);
+    quad[2].texCoords = sf::Vector2f((randNb + 1) * TILEWIDTH, TILEWIDTH);
+    quad[3].texCoords = sf::Vector2f((randNb) * TILEWIDTH, TILEWIDTH);
 
     for (int i = 0; i < 4; ++i) {
       m_vertices.append(quad[i]);
@@ -42,16 +53,17 @@ namespace huaca {
     sf::Vertex quad[4];
 
     // Define position
-    quad[0].position = sf::Vector2f(coordTile.x * TILE_SIZE, coordTile.y * TILE_SIZE);
-    quad[1].position = sf::Vector2f((coordTile.x + 1) * TILE_SIZE, coordTile.y * TILE_SIZE);
+    quad[0].position = sf::Vector2f(coordTile.x * TILE_SIZE, coordTile.y * TILE_SIZE + TILE_SIZE/2);
+    quad[1].position = sf::Vector2f((coordTile.x + 1) * TILE_SIZE, coordTile.y * TILE_SIZE + TILE_SIZE/2);
     quad[2].position = sf::Vector2f((coordTile.x + 1) * TILE_SIZE, (coordTile.y + 1) * TILE_SIZE);
     quad[3].position = sf::Vector2f(coordTile.x * TILE_SIZE, (coordTile.y + 1) * TILE_SIZE);
 
-    // Define color (DEBUG)
-    quad[0].color = sf::Color(0x80, 0x80, 0x80);
-    quad[1].color = sf::Color(0x80, 0x80, 0x80);
-    quad[2].color = sf::Color(0x80, 0x80, 0x80);
-    quad[3].color = sf::Color(0x80, 0x80, 0x80);
+    // Define texture
+    int randNb = m_random.computeUniformInteger(0,4);
+    quad[0].texCoords = sf::Vector2f((randNb) * TILEWIDTH, TILEWIDTH);
+    quad[1].texCoords = sf::Vector2f((randNb + 1) * TILEWIDTH, TILEWIDTH);
+    quad[2].texCoords = sf::Vector2f((randNb + 1) * TILEWIDTH, TILEHEIGHT + TILEWIDTH);
+    quad[3].texCoords = sf::Vector2f((randNb) * TILEWIDTH, TILEHEIGHT + TILEWIDTH);
 
     for (int i = 0; i < 4; ++i) {
       m_vertices.append(quad[i]);
@@ -63,7 +75,9 @@ namespace huaca {
   }
 
   void GroundManager::render(sf::RenderWindow& window) {
-    window.draw(m_vertices);
+    sf::RenderStates states;
+    states.texture = m_groundsWallsTexture;
+    window.draw(m_vertices, states);
   }
 
 }
